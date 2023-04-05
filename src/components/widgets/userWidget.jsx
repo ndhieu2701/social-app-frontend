7c702d40ec3d33c0d48960e61682803eb2364fcf
ndhieu2701 <hieu@v7rx3.onmicrosoft.com>
2023-04-05 17:06:25 +0700

    update user widget

--- src/components/widgets/userWidget.jsx
+++ src/components/widgets/userWidget.jsx
@@ -1,10 +1,9 @@
 import {
   ManageAccountsOutlined,
-  EditOutlined,
   LocationOnOutlined,
   WorkOutlineOutlined,
 } from "@mui/icons-material";
-import { Box, Typography, Divider, useTheme } from "@mui/material";
+import { Box, Typography, Divider, useTheme, IconButton } from "@mui/material";
 import UserImage from "../userImage";
 import FlexBetween from "../flexBetween";
 import WidgetWrapper from "./widgetWrapper";
@@ -13,11 +12,9 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-import linkedin from "../../assets/linkedin.png"
-import twitter from "../../assets/twitter.png"
-
 const UserWidget = ({ userId, picturePath }) => {
   const [user, setUser] = useState(null);
+  const { _id } = useSelector((state) => state.user.user);
   const { palette } = useTheme();
   const navigate = useNavigate();
   const token = useSelector((state) => state.user.token);
@@ -39,25 +36,13 @@ const UserWidget = ({ userId, picturePath }) => {
 
   if (!user) return null;
 
-  const {
-    firstName,
-    lastName,
-    location,
-    occupation,
-    viewedProfile,
-    impressions,
-    friends,
-  } = user;
+  const { firstName, lastName, location, occupation, friends } = user;
 
   return (
     <WidgetWrapper>
       {/* FIRST ROW */}
-      <FlexBetween
-        gap="0.5rem"
-        pb="1.1rem"
-        onClick={() => navigate(`/profile/${userId}`)}
-      >
-        <FlexBetween gap="1rem">
+      <FlexBetween gap="0.5rem" pb="1.1rem">
+        <FlexBetween gap="1rem" onClick={() => navigate(`/profile/${userId}`)}>
           <UserImage image={picturePath} />
           <Box>
             <Typography
@@ -76,7 +61,11 @@ const UserWidget = ({ userId, picturePath }) => {
             <Typography color={medium}>{friends.length} friends</Typography>
           </Box>
         </FlexBetween>
-        <ManageAccountsOutlined />
+        {userId === _id && (
+          <IconButton onClick={() => navigate("/settings")}>
+            <ManageAccountsOutlined />
+          </IconButton>
+        )}
       </FlexBetween>
 
       <Divider />
@@ -91,59 +80,6 @@ const UserWidget = ({ userId, picturePath }) => {
           <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
           <Typography color={medium}>{occupation}</Typography>
         </Box>
-      </Box>
-
-      <Divider />
-
-      {/* THIRD ROW */}
-      <Box p="1rem 0">
-        <FlexBetween mb="0.5rem">
-          <Typography color={medium}>Who's viewed your profile</Typography>
-          <Typography color={main} fontWeight="500">
-            {viewedProfile}
-          </Typography>
-        </FlexBetween>
-        <FlexBetween>
-          <Typography color={medium}>Impressions of your post</Typography>
-          <Typography color={main} fontWeight="500">
-            {impressions}
-          </Typography>
-        </FlexBetween>
-      </Box>
-
-      <Divider />
-
-      {/* FOURTH ROW */}
-      <Box p="1rem 0">
-        <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
-          Social Profiles
-        </Typography>
-
-        <FlexBetween gap="1rem" mb="0.5rem">
-          <FlexBetween gap="1rem">
-            <img src={twitter} alt="twitter" />
-            <Box>
-              <Typography color={main} fontWeight="500">
-                Twitter
-              </Typography>
-              <Typography color={medium}>Social Network</Typography>
-            </Box>
-          </FlexBetween>
-          <EditOutlined sx={{ color: main }} />
-        </FlexBetween>
-
-        <FlexBetween gap="1rem">
-          <FlexBetween gap="1rem">
-            <img src={linkedin} alt="linkedin" />
-            <Box>
-              <Typography color={main} fontWeight="500">
-                Linkedin
-              </Typography>
-              <Typography color={medium}>Network Platform</Typography>
-            </Box>
-          </FlexBetween>
-          <EditOutlined sx={{ color: main }} />
-        </FlexBetween>
       </Box>
     </WidgetWrapper>
   );
